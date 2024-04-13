9d79f18c28cfd3d7361e23935cd3720784a0a0a2
Dmytro Chekhuta <chehuta1996@gmail.com>
2024-04-13 12:32:42 +0300

    init: init url simplifier task

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh', 'react-hooks', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'warn',
+    "react/react-in-jsx-scope": "off",
+  },
+}
---
+++ .prettierrs.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 4,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
