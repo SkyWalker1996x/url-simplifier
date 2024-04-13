ec088f47730c117c16f0cd2317535ca4f7975ca4
Dmytro Chekhuta <chehuta1996@gmail.com>
2024-04-13 12:39:28 +0300

    feat: create description component

---
+++ src/features/url-simplifier/components/description/index.tsx
@@ -0,0 +1,13 @@
+import classes from 'features/url-simplifier/components/description/styles.module.css';
+
+const Description = () => {
+  return (
+    <div className={classes.description}>
+      URL Simplifier for transforming long, ugly links into nice, memorable and trackable short URLs. Use it to
+      shorten links for any social media platforms, blogs, SMS, emails, ads, or pretty much anywhere else you want to
+      share them. Twitter, Facebook, YouTube, Instagram, WhatsApp, emails, SMS, videos.
+    </div>
+  );
+};
+
+export default Description;
